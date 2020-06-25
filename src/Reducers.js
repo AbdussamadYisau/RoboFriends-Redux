6167dffe3c4fcc02f310ed70d03bb99578ed64b0
AbdussamadYisau <Sammieyisau@gmail.com>
2020-06-26 00:40:33 +0100

    Progress so far

---
+++ src/Reducers.js
@@ -0,0 +1,16 @@
+import {CHANGE_SEARCH_FIELD} from './Constants';
+
+const initialState = {
+    searchField: ''
+}
+
+
+// Reducer Function
+
+export const searchRobots = (state=initialState, action = {}) => {
+    switch(action.type) {
+        case CHANGE_SEARCH_FIELD:
+
+
+    }
+}
